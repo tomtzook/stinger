718ab1d18100185dd35dcc6aa6576c37b38bea98
Tom Tzook <tomtzook@gmail.com>
2022-06-06 20:36:31 +0300

    new feature apps

--- server/src/main/java/com/stinger/server/commands/CommandProcessor.java
+++ server/src/main/java/com/stinger/server/commands/CommandProcessor.java
@@ -1,5 +1,7 @@
 package com.stinger.server.commands;
 
+import com.castle.nio.zip.OpenZip;
+import com.castle.nio.zip.Zip;
 import com.stinger.framework.commands.CommandDefinition;
 import com.stinger.framework.commands.CommandType;
 import com.stinger.framework.commands.GenericCommandType;
@@ -7,9 +9,12 @@ import com.stinger.framework.commands.Parameters;
 import com.stinger.framework.data.KnownTypes;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 public class CommandProcessor {
 
@@ -22,17 +27,35 @@ public class CommandProcessor {
     public CommandDefinition processCommand(RawCommandDef def) throws IOException {
         CommandType commandType = mCommandTypes.getFromKey(def.getType());
         switch (commandType.intValue()) {
-            case 20: {
-                Path path = Paths.get((String) def.getParams().get("path"));
-                byte[] bytes = Files.readAllBytes(path);
-                def.getParams().put("codeBuffer", bytes);
+            case 20:
+                processInstallAppCommand(def);
                 break;
-            }
         }
 
         return new CommandDefinition(
                 commandType,
                 new Parameters(def.getParams())
         );
+    }
+
+    private void processInstallAppCommand(RawCommandDef def) throws IOException {
+        Path path = Paths.get((String) def.getParams().get("path"));
+
+        Zip zip = Zip.fromPath(path);
+        try (OpenZip openZip = zip.open()) {
+            Path manifestPath = openZip.getPath("META-INF", "Manifest.mf");
+            try (InputStream inputStream = Files.newInputStream(manifestPath)) {
+                Manifest manifest = new Manifest(inputStream);
+                Attributes attributes = manifest.getMainAttributes();
+                int id = (int) attributes.get("appId");
+                String version = (String) attributes.get("appVersion");
+
+                def.getParams().put("app-id", id);
+                def.getParams().put("app-version", version);
+            }
+        }
+
+        byte[] bytes = Files.readAllBytes(path);
+        def.getParams().put("codeBuffer", bytes);
     }
 }
